2834d992f05de7c2ff9ce6dba436636508d5de1e
Nagy Szabolcs Vencel <123639061+nonsensei013@users.noreply.github.com>
2025-09-24 10:18:03 +0200

    BasicQC first commit

---
+++ basicQC/docker_push.sh
@@ -0,0 +1,17 @@
+TOOL=basicqc
+DOCKER_USER=deptmetagenom
+VERSION="1.1.0"
+DOCKER_IMAGE=${DOCKER_USER}/${TOOL}:${VERSION}
+DOCKER_LATEST=${DOCKER_USER}/${TOOL}:latest
+
+# optional to clean up previous docker images:
+# sudo docker rm -vf $(sudo docker ps -aq)
+# sudo docker rmi -f $(sudo docker images -aq)
+# sudo docker system prune -a
+
+sudo docker build -t ${TOOL} .
+
+sudo docker tag ${TOOL} ${DOCKER_IMAGE}
+sudo docker tag ${TOOL} ${DOCKER_LATEST}
+sudo docker push ${DOCKER_IMAGE}
+sudo docker push ${DOCKER_LATEST}
